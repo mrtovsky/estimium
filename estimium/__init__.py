4e2993a8756f67529dada214a730e1ff0cc5fa51
mrtovsky <emerte.quant@gmail.com>
2020-03-29 19:11:21 +0200

    :tada: Initialize repository with Poetry
    Create the Professor Estimium package with Poetry - tool for Python
    dependency management and packaging.

---
+++ estimium/__init__.py
@@ -0,0 +1,11 @@
+"""
+Machine Learning auxiliary modules for Python
+=============================================
+
+Professor Estimium provides tools and scikit-learn compatible estimators
+extending functionalities of deep-rooted packages aimed to replace ML
+boilerplates with a simple API and universal implementation.
+"""
+
+
+__version__ = "0.1.0"
---
+++ tests/test_estimium.py
@@ -0,0 +1,12 @@
+"""
+This module gathers tests related to the release of the new version of
+Professor Estimium package.
+"""
+
+
+from estimium import __version__
+
+
+def test_version():
+    """Test version number of released package."""
+    assert __version__ == "0.1.0"
