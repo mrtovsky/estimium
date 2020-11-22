7f2e315eaae51e4267d707aa1a44c1cc9ce98553
mrtovsky <mrtovsky@protonmail.com>
2020-11-22 11:50:54 +0100

    :memo: Fix docstring formatting up to 72 chars per line

--- estimium/base.py
+++ estimium/base.py
@@ -5,7 +5,6 @@ import logging
 from abc import ABCMeta, abstractmethod
 from typing import Optional
 
-
 __all__ = ["ClassLogger"]
 
 
@@ -15,20 +14,21 @@ class ClassLogger(metaclass=ABCMeta):
     Parameters
     ----------
     logger : object of logging.Logger, optional (default=None)
-        Logging object for which a descendant class logger will be assigned.
-        By default creates a new object named according to the module name
-        where the object of this class was instantiated, using
-        `self.__module__` name. That's why it is important to follow the
-        recommended logging naming convention and name loggers using `__name__`
-        special variable.
+        Logging object for which a descendant class logger will be
+        assigned. By default creates a new object named according to the
+        module name where the object of this class was instantiated,
+        using `self.__module__` name. That's why it is important to
+        follow the recommended logging naming convention and name
+        loggers using `__name__` special variable.
 
     Warnings
     --------
     This class should not be used directly. Use derived classes instead.
 
-    It is also not suitable for being a parent class of the custom scikit-learn
-    estimators because it violates the rule of not placing logic into
-    instantiation. See more at `Developing scikit-learn estimators
+    It is also not suitable for being a parent class of the custom
+    scikit-learn estimators because it violates the rule of not placing
+    logic into instantiation. See more at `Developing scikit-learn
+    estimators
     <https://scikit-learn.org/stable/developers/develop.html>`_.
 
     """
